95040d165d614e1355631a44f62ceca23b60f305
vvvtoom@gmail.com <vvvtoom@gmail.com>
2024-02-07 23:14:24 +0900

    I'm fool

--- boardgame/urls.py
+++ boardgame/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("api_init_game/", views.api_init_game, name='api_init_game'),
     path("api_add_player/", views.api_add_player, name='api_add_player'),
     path("api_get_players/", views.api_get_players, name='api_get_players'),
+    path("api_get_my_money/<str:player_name>/", views.api_get_my_money, name='api_get_my_money'),
     path("api_send_money/", views.api_send_money, name='api_send_money'),
     path("api_plus_money/", views.api_plus_money, name='api_plus_money'),
     path("api_minus_money/", views.api_minus_money, name='api_minus_money'),
